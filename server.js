56bcca47fc8d038fe7b1a6614794fc76a0488c40
Jarett Lund-Hopkins <jarettlh@gmail.com>
2020-02-27 14:26:49 -0800

    fix db connect error

--- server.js
+++ server.js
@@ -2,13 +2,38 @@ const express = require('express');
 const app = express();
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var db_config = {
   host: "us-cdbr-iron-east-04.cleardb.net",
   user: "bc2b6d33a1bb4f",
   password: "e46585b9",
   database: "heroku_eb75923b7ca9e4b"
-});
-con.connect();
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
